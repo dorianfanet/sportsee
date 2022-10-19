3f41e67757f780710c32b2aee4e12ce355d301d3
dorianfanet <dorian.fanet@gmail.com>
2022-10-19 17:09:20 +0200

    initial commit

---
+++ src/components/Aside.jsx
@@ -0,0 +1,61 @@
+import icon1 from '../assets/icon1.svg'
+import icon2 from '../assets/icon2.svg'
+import icon3 from '../assets/icon3.svg'
+import icon4 from '../assets/icon4.svg'
+import styled from 'styled-components'
+
+const AsideContainer = styled.aside`
+  margin: 0;
+  width: 117px;
+  z-index: -1;
+  position: fixed;
+  top: 0;
+  height: calc(100vh);
+  background-color: var(--secondaryColor);
+  display: grid;
+  place-content: center;
+
+  & ul{
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+
+    & li{
+      list-style-type: none;
+      width: 64px;
+      height: 64px;
+      background-color: white;
+      border-radius: 6px;
+      display: grid;
+      place-content: center;
+      cursor: pointer;
+    }
+  }
+
+  & p{
+    position: absolute;
+    color: white;
+    bottom: 80px;
+    left: 50%;
+    transform: translateX(-50%) rotate(-90deg);
+    font-size: 12px;
+    height: 12px;
+    white-space: nowrap;
+  }
+`
+
+export default function Aside() {
+  return (
+    <AsideContainer>
+      <ul>
+        <li><img src={icon1} alt="" /></li>
+        <li><img src={icon2} alt="" /></li>
+        <li><img src={icon3} alt="" /></li>
+        <li><img src={icon4} alt="" /></li>
+      </ul>
+      <p>Copiryght, SportSee 2020</p>
+    </AsideContainer>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import logo from '../assets/logo.svg'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  padding: 0 30px;
+  height: 91px;
+  background-color: var(--secondaryColor);
+  position: fixed;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  z-index: 100;
+
+  & img{
+    height: 57px;
+  }
+
+  & nav{
+    width: calc(100% - 246px);
+    display: flex;
+    justify-content: space-around;
+
+    & a{
+      color: white;
+      font-size: 24px;
+      font-weight: 500;
+    }
+  }
+`
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="" />
+      <nav>
+        <Link to='/'>Accueil</Link>
+        <Link to='profile'>Profil</Link>
+        <Link to='settings'>Réglages</Link>
+        <Link to='community'>Communauté</Link>
+      </nav>
+    </HeaderContainer>
+  )
+}
